618c721b9ebc0d01d5b53ca5af4d2299fd0a487e
Devlin Duldulao <devlin.duldulao@inmeta.no>
2021-01-03 11:09:49 +0100

    updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="container w-100 d-flex flex-row justify-content-center align-items-lg-center">
-      <h1>Welcome to Mobx 6 Course 🧑‍🏫 💻</h1>
+      <h1>Welcome to Mobx State Tree Course 🧑‍🏫 💻</h1>
     </div>
   );
 };
